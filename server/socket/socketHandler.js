6f20d11da398d022d8fcc2ffdec10a99984b4024
Amish kumar <157312626+amish-kumar-07@users.noreply.github.com>
2025-05-07 08:16:15 +0530

    Update socketHandler.js

--- server/socket/socketHandler.js
+++ server/socket/socketHandler.js
@@ -1,42 +1,43 @@
-// sockethandler.js
-const { Server } = require("socket.io");
+import { Server } from "socket.io";
+import {webrtcServices} from "./webrtcservices";
 
-const NametoSocketId = new Map();
-const SockettoName = new Map();
+function socketHandler(server) {
+  const io = new Server(server, {
+    cors: {
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST"]
+    }
+  });
 
-const initSocket = (server) => {
-  const io = new Server(server, { cors: true });
+  const nameToSocket = new Map();
+  const socketToName = new Map();
 
   io.on("connection", (socket) => {
     console.log("A user connected:", socket.id);
 
-    socket.on("join-room", (data) => {
-      const { roomId, Name } = data;
-      console.log("User:", Name, "joined room:", roomId);
-      NametoSocketId.set(Name, socket.id);
-      SockettoName.set(socket.id, Name);
+    socket.on("join-room", ({ roomId, name }) => {
+      console.log(`${name} joined room: ${roomId}`);
       socket.join(roomId);
-      socket.broadcast.to(roomId).emit("user-joined", { Name });
+      nameToSocket.set(name, socket.id);
+      socketToName.set(socket.id, name);
+
+      // Notify all members in the room
+      socket.broadcast.to(roomId).emit("user-joined", { name });
       socket.emit("joined-room", { roomId });
     });
 
-    socket.on("call-user", (data) => {
-      const { Name, offer } = data;
-      const socketId = NametoSocketId.get(Name);
-      const from = SockettoName.get(socket.id);
-      socket.to(socketId).emit("incoming-call", { from, offer });
-    });
+    // WebRTC signaling 
+    webrtcServices(io, socket, nameToSocket, socketToName);
 
-    socket.on("offer", (data) => {
-      socket.to(data.roomId).emit("offer", { offer: data.offer, userId: socket.id });
-    });
-
-    socket.on("answer", (data) => {
-      socket.to(data.roomId).emit("answer", { answer: data.answer, userId: socket.id });
+    socket.on("disconnect", () => {
+      const name = socketToName.get(socket.id);
+      if (name) {
+        nameToSocket.delete(name);
+        socketToName.delete(socket.id);
+        console.log(`${name} disconnected`);
+      }
     });
   });
+}
 
-  return io;
-};
-
-module.exports = { initSocket }; 
+module.exports = socketHandler;
